93f7981ccaf6c4a6671a1d7ebd2b9e79d2c508f5
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2025-11-09 19:19:09 +0100

    destructuring fix
    Co-Authored-By: Marcelo Domínguez <69964857+Sa4dUs@users.noreply.github.com>

--- src/router.js
+++ src/router.js
@@ -15,9 +15,7 @@ router.get("/", async (req, res) => {
 	const page = Math.max(parseInt(req.query.page) || 1, 1);
 	const pageSize = 3;
 
-  	const pagination = await service.getGamesPaginated(page, pageSize);
-  	const games = pagination.docs || [];
-  	const totalPages = pagination.totalPages || 1;
+  	const {docs:games=[], totalPages=1} = await service.getGamesPaginated(page, pageSize); 
 
   	const prevPage = Math.max(page-1,1);
   	const nextPage = Math.min(page+1,totalPages);
