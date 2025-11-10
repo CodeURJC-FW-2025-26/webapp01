6293b216a9fa0da57e10ff09fef492dcb44f4718
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2025-11-10 11:46:58 +0100

    Renamed

--- src/router.js
+++ src/router.js
@@ -3,8 +3,8 @@ import multer from "multer";
 
 import * as service from "./service.js";
 import * as gameHandler from "./handlers/games.js";
-import * as gameDetailHandler from "./handlers/gameDetails.js";
-import * as formHandler from "./handlers/formHandler.js";
+import * as gameDetailHandler from "./handlers/detail.js";
+import * as formHandler from "./handlers/form.js";
 
 const router = express.Router();
 export const PAGE_SIZE = 6;
