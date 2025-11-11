1e99f50381190185d3dc1431ce4b3e8734723049
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2025-11-11 19:52:04 +0100

    CSS issue

--- src/handlers/confirm.js
+++ src/handlers/confirm.js
@@ -1,6 +1,6 @@
 
 export const getConfirmation = async (req, res) => {
 	const backUrl=req.query.back||"/";
-	const UrlOfDestination=req.query.destination||"/"; 
-	res.render("confirmation", { UrlOfDestination, backUrl });
+	const urlOfDestination=req.query.destination||"/"; 
+	res.render("confirm", { urlOfDestination, backUrl  });
 };
