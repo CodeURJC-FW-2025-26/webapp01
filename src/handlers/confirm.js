d1f4df7dcc8e0edeeb31b616964abe17ca067a56
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2025-11-11 19:03:01 +0100

    Update confirm.js

--- src/handlers/confirm.js
+++ src/handlers/confirm.js
@@ -1,6 +1,6 @@
 
 export const getConfirmation = async (req, res) => {
 	const backUrl=req.query.back||"/";
-	const UrlOfDestination=req.query.back||"/"; //FIXME
+	const UrlOfDestination=req.query.destination||"/"; 
 	res.render("confirmation", { UrlOfDestination, backUrl });
 };
--- src/handlers/error.js
+++ src/handlers/error.js
@@ -1,6 +1,6 @@
 
 export const getError = async (req, res) => {
 	const errorMessage = req.query.type || "An unknown error occurred.";
-	const backUrl = req.query.back||"/"; //FIXME
+	const backUrl = req.query.back||"/"; //WATCH OUT
 	res.render("error", { errorMessage, backUrl });
 };
