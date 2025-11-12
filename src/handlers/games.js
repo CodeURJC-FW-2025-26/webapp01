517f5ed5c011d850b3918f245d6bd3f773a4ac09
Marcelo Domínguez <dmmarcelo27@gmail.com>
2025-11-12 18:19:37 +0100

    Add search feature to main page

--- src/handlers/games.js
+++ src/handlers/games.js
@@ -3,10 +3,13 @@ import { PAGE_SIZE } from "../router.js";
 
 
 export const getPaginatedGames = async (req, res) => {
+	const search = req?.query?.search ?? "";
+	const filter = { title: { $regex: search, $options: "i" } }
+
 	const parsed = parseInt(req.query.page);
 	const page = Number.isNaN(parsed) ? 1 : Math.max(parseInt(req.query.page), 1);
 
-  	const { docs: games = [], totalPages = 1 } = await service.getGamesPaginated(page, PAGE_SIZE); 
+  	const { docs: games = [], totalPages = 1 } = await service.getGamesPaginated(page, PAGE_SIZE, filter); 
 
   	const prevPage = Math.max(page - 1, 1);
   	const nextPage = Math.min(page + 1, totalPages);
