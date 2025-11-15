7a1763b2d27c75528204e48b254b4cddca38bfe5
Alejandro García Prada <114813960+AlexGarciaPrada@users.noreply.github.com>
2025-11-15 16:09:18 +0100

    some post routes

--- src/handlers/confirm.js
+++ src/handlers/confirm.js
@@ -1,6 +1,17 @@
 
+import { Monkito } from "./lib/monkito.js";
+
 export const getConfirmation = async (req, res) => {
 	const backUrl=req.query.back||"/";
 	const urlOfDestination=req.query.destination||"/"; 
 	res.render("confirm", { urlOfDestination, backUrl  });
 };
+
+
+router.post('/submit-game-confirmation',(req, res) => {
+
+});
+
+router.post('/cancel-game-confirmation',(req,res)=>{
+	return res.render("form",req)
+})
--- src/handlers/form.js
+++ src/handlers/form.js
@@ -1,4 +1,23 @@
+import router from "./router.js";
 
 export const getForm = (req, res) => {
 	res.render("form");
 };
+
+
+router.post('/submit-game-form',(req, res) => {
+	//To check that there are not empty/invalid fields
+	for (let key in req.body){
+
+		if (typeof req.body[key] === "string")
+			req.body[key] = req.body.trim();
+
+		if (!req.body[key])
+		{
+			return res.render("error",{type:"Some of the form fields were invalid"});
+		}
+	}
+
+	return res.render("confirm",{data:req});
+
+});
