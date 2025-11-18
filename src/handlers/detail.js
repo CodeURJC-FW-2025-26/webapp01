bb0f62c5276d840bd47e924f8ac23ad3fc4e8034
Alejandro García Prada <114813960+AlexGarciaPrada@users.noreply.github.com>
2025-11-19 00:29:20 +0100

    date hotfix

--- src/handlers/detail.js
+++ src/handlers/detail.js
@@ -1,11 +1,24 @@
 import * as service from "../service.js";
 import { toObjectId } from "../lib/monkito.js";
 
+export const formatDateDDMMYYYY = (dateString) => {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    if (isNaN(date)) return "";
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+};
+
 export const getGameDetail = async (req, res) => {
 	const id = req.params.id ?? 0;
 	let game = await service.getGame(id);
 	game = {
 		...game,
+		release_date: formatDateDDMMYYYY(game.release_date),
 		average_rating:
             game.reviews.reduce((acc, e) => e.rating + acc, 0) /
             game.reviews.length,
